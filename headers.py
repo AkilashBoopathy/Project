370fced435271e4ea5ae51d0d15872ab0c3bc95d
AkilashBoopathy <104289048+AkilashBoopathy@users.noreply.github.com>
2023-04-26 13:09:08 +0530

    Update headers.py

--- headers.py
+++ headers.py
@@ -1,20 +1,20 @@
 import urllib
 import re
 import time
-from urllib import FancyURLopener
+from urllib.request import FancyURLopener
 
 class colors:
-        def __init__(self):
-                self.green = "\033[92m"
-                self.blue = "\033[94m"
-                self.bold = "\033[1m"
-                self.yellow = "\033[93m"
-                self.red = "\033[91m"
-                self.end = "\033[0m"
+    def __init__(self):
+        self.green = "\033[92m"
+        self.blue = "\033[94m"
+        self.bold = "\033[1m"
+        self.yellow = "\033[93m"
+        self.red = "\033[91m"
+        self.end = "\033[0m"
 ga = colors()
 
 class UserAgent(FancyURLopener):
-	version = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0'
+    version = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0'
 
 useragent = UserAgent()
 
@@ -23,22 +23,21 @@ class HTTP_HEADER:
     SERVER = "Server"
 
 def headers_reader(url):
-	# This function will print the server headers such as WebServer OS & Version.
-	print ga.bold+" \n [!] Fingerprinting the backend Technologies."+ga.end
-	opener = urllib.urlopen(url)
-	if opener.code == 200:
-		 print ga.green+" [!] Status code: 200 OK"+ga.end
-	if opener.code == 404:
-		 print ga.red+" [!] Page was not found! Please check the URL \n"+ga.end
-		 exit()
-	#Host = opener.headers.get(HTTP_HEADER.HOST)
-	Server = opener.headers.get(HTTP_HEADER.SERVER)
-	# HOST will split the HostName from the URL
-	Host = url.split("/")[2]
-	print ga.green+" [!] Host: " + str(Host) +ga.end
-	print ga.green+" [!] WebServer: " + str(Server) +ga.end
-	for item in opener.headers.items():
-	    for powered in item:
-		sig = "x-powered-by"		
-		if sig in item:
-		    print ga.green+ " [!] " + str(powered).strip() + ga.end
+    # This function will print the server headers such as WebServer OS & Version.
+    print(ga.bold+" \n [!] Fingerprinting the backend Technologies."+ga.end)
+    opener = useragent.open(url)
+    if opener.code == 200:
+        print(ga.green+" [!] Status code: 200 OK"+ga.end)
+    if opener.code == 404:
+        print(ga.red+" [!] Page was not found! Please check the URL \n"+ga.end)
+        exit()
+    #HOST = opener.headers.get(HTTP_HEADER.HOST)
+    Server = opener.headers.get(HTTP_HEADER.SERVER)
+    # HOST will split the HostName from the URL
+    Host = url.split("/")[2]
+    print(ga.green+" [!] Host: " + str(Host) +ga.end)
+    print(ga.green+" [!] WebServer: " + str(Server) +ga.end)
+    for item in opener.headers.items():
+        sig = "x-powered-by"
+        if sig in item[0]:
+            print(ga.green+ " [!] " + str(item).strip() + ga.end)
